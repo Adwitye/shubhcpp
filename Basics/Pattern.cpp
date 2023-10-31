c48aebee55843217bbe1efd435be702fc453843e
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2023-10-31 18:09:53 +0000

    Pattern

--- Basics/Pattern.cpp
+++ Basics/Pattern.cpp
@@ -5,10 +5,20 @@ int main(){
     cout<<"Enter the no.of rows";
     cin >>n;
     for(int row=0; row<n;row=row+1){
-        for(int col=0; col<row+1; col=col+1){
-            cout<<row+col+1<<" ";
+        for(int j=0;j<n-row+3;j++){
+            cout<<" ";
         }
+        for(int col=0; col<row+1; col=col+1){
+            cout<<row+col+1<<"";
+        }
+        int a=row*2;
+        for(int j=0; j<row; j++){
+            cout<<a;
+            a--;
+        }
+
         cout<< endl;
+        
     }
 
 }
