7b8335ecb6cf8d27be8089fd1696540071e79a70
Aditya Shubham Jha <117833600+Adwitye@users.noreply.github.com>
2023-11-01 18:52:09 +0000

    operator

---
+++ Basics/operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool a = true;
+    bool b = false;
+    // cout<< a << endl;
+    // cout<< !a << endl;
+
+    //left shift
+    int c; 
+    int v = 12;
+    // v = v<<1; //24
+    c=v<<2;
+    cout<< c << endl;
+
+}
